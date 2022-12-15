42afbf44a5e364f1dc9d566e5d39e013600d91cc
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-12-14 23:43:48 -0300

    feat: implementando conceito de repositorio em memoria para testes

--- ignite-lab-microservices/notifications-service/src/domain/use-cases/send-notification.spec.ts
+++ ignite-lab-microservices/notifications-service/src/domain/use-cases/send-notification.spec.ts
@@ -1,23 +1,17 @@
-import Notification from "../entities/notifications/notification";
+import InMemoryNotificationsRepository from "../../../test/repositories/in-memory-notifications-repository";
 import SendNotification from "./send-notification";
-
-const notifications: Notification[] = [];
-
-const fakeNotificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should send a notification', async () => {
+    const fakeNotificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(fakeNotificationsRepository);
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       recipientId: '123',
       content: 'Hello world',
       category: 'test',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(fakeNotificationsRepository.notifications).toHaveLength(1);
+    expect(fakeNotificationsRepository.notifications[0]).toEqual(notification);
   });
 });
---
+++ ignite-lab-microservices/notifications-service/test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,10 @@
+import Notification from "../../src/domain/entities/notifications/notification";
+import NotificationsRepository from "../../src/domain/repositories/notifications-repository";
+
+export default class InMemoryNotificationsRepository implements NotificationsRepository {
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification) {
+    this.notifications.push(notification);
+  }
+}
