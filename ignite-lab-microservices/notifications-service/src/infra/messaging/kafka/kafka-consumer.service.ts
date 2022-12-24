ef93969686f2b288dd3ec18a7e224374bdf320d5
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-12-24 14:51:34 -0300

    feat: implementando consumer kafka

---
+++ ignite-lab-microservices/notifications-service/src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy } from "@nestjs/common";
+import { ServerKafka } from "@nestjs/microservices";
+
+@Injectable()
+export class KafkaConsumerService extends ServerKafka implements OnModuleDestroy {
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications-service',
+        brokers: [process.env.KAFKA_BROKER],
+        sasl: {
+          mechanism: "scram-sha-256",
+          username: process.env.KAFKA_SASL_USERNAME,
+          password: process.env.KAFKA_SASL_PASSWORD,
+        },
+        ssl: true,
+      }
+    })
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
---
+++ ignite-lab-microservices/notifications-service/src/infra/messaging/messasing.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { NotificationsController } from "./kafka/controllers/notifications.controller";
+import { KafkaConsumerService } from "./kafka/kafka-consumer.service";
+
+@Module ({
+  imports: [],
+  providers: [KafkaConsumerService],
+  controllers: [NotificationsController],
+})
+
+export class MessagingModule {}
